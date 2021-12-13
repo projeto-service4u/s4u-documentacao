e83b1ce4483e2c73f965773fa44a0f61ade34fc6
iurycardoso <iurycardoso6@gmail.com>
2021-12-12 21:32:59 -0300

    Finalizacao da documentacao

--- s4u-wiki/docusaurus.config.js
+++ s4u-wiki/docusaurus.config.js
@@ -24,14 +24,9 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+          editUrl: "https://github.com/tccService4u/Service4u",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
-        },
+
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -57,7 +52,7 @@ const config = {
           },
 
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/tccService4u/Service4u",
             label: "GitHub",
             position: "right",
           },
@@ -76,32 +71,11 @@ const config = {
             ],
           },
           {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/tccService4u/Service4u",
               },
             ],
           },
--- s4u-wiki/src/pages/index.md
+++ s4u-wiki/src/pages/index.md
@@ -6,4 +6,44 @@ hide_table_of_contents: true
 
 # Service 4 YOU
 
-Aqui vai ser o conteúdo do meu s4u
+# Como rodar o projeto
+
+### 1 - Efetuar o clone do projeto
+
+```tsx
+git clone https://github.com/tccService4u/Service4u.git
+```
+
+### 2 - Navegar até a raiz do projeto e instalar as dependências com o comando:
+
+```bash
+yarn
+```
+
+após esse comando todas as dependências serão instaladas.
+
+---
+
+### 3 - Configurar o Firebase conforme o vídeo:
+
+[![Watch the video](https://i.imgur.com/ekb7S51l.png)](https://youtu.be/CATYchtAn70)
+
+### 4 - Configurar o login e senha da aplicação no Firebase conforme o vídeo:
+
+[![Watch the video](https://i.imgur.com/cKVDuyFl.png)](https://youtu.be/-2m20Ap9td4)
+
+### 5 - Configurar o EmailJS conforme o vídeo:
+
+[![Watch the video](https://i.imgur.com/eQcqORkl.png)](https://youtu.be/N1U2knCF-5I)
+
+### 6 - Rodar a aplicação com o comando:
+
+```bash
+yarn start
+```
+
+### 7 - Se deu tudo certo a aplicação vai estar acessível no endereço:
+
+```bash
+http://localhost:3000/
+```
